7669a99a321a38cc7b6670ee327d48bbc197884c
camfeghali <camfeg@gmail.com>
2021-06-01 03:24:14 -0700

    renders text inputs properly

---
+++ webapp/Form_Test.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Form_Test.aspx.cs" Inherits="Form_Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <link href="Content/bootstrap.css" rel="stylesheet"/>
+    <link href="Content/jquery-ui-1.11.4.min.css" rel="stylesheet" />
+    <link href="Content/Site.css" rel="stylesheet" />
+    <link href="Content/webapp.css" rel="stylesheet" />
+
+    <script src="Scripts/jquery-2.2.3.min.js"></script>
+    <script src="Scripts/jquery-ui-1.11.4.min.js"></script>
+    <script src="Scripts/webapp.js"></script>
+</head>
+<body>
+    <div id="template-form-wrapper">
+        <h1>Competitive Pricing Form Template</h1>
+        <form id="template-form">
+            <asp:Literal ID="FormContent" runat="server" />
+        </form>
+    </div>
+</body>
+</html>
---
+++ webapp/Form_Test.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApp.App_Code;
+
+public partial class Form_Test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GlobalClass gc = new GlobalClass();
+        DataTable dt = new DataTable();
+        DataTableCollection dtc = null;
+        StringBuilder sb = new StringBuilder();
+
+        dtc = gc.GetTables("exec dcr_sp_form 1");
+        dt = dtc[0];
+
+        IRenderer currentRenderer;
+
+        foreach (DataRow row in dt.Rows)
+        {
+            currentRenderer = MapTypeToInputRenderer(row["Type"].ToString());
+            var dataFields = new Dictionary<string, string>(){
+                {"TemplateFieldId", row["Template_Field_ID"].ToString()},
+                {"FieldName", row["Field"].ToString()},
+                {"FieldDescription", row["Template_Field_Description"].ToString()},
+                {"Required", row["Required"].ToString()}
+            };
+            sb.AppendLine(currentRenderer.Draw(dataFields));
+        }
+
+        FormContent.Text = $"{sb}";
+    }
+
+    private IRenderer MapTypeToInputRenderer(string type)
+    {
+        switch (type)
+        {
+            case "Text 100 characters":
+                return new TextInput();
+            default:
+                return new TextInput();
+        }
+    }
+
+    interface IRenderer
+    {
+        string Draw(Dictionary<string, string> DataFields);
+    }
+
+    public class TextInput : IRenderer
+    {
+        public string Draw(Dictionary<string, string> DataFields)
+        {
+            return $@"<div class='mb-3'>
+                        <label for='{DataFields["FieldName"]}' class='form-label d-flex flex-row align-items-baseline justify-content-between'>
+                            {DataFields["FieldName"]}
+                            <div class='form-text text-muted'>{DataFields["FieldDescription"]}.</div>
+                        </label>
+                        <input type='text' class='form-control' id='{DataFields["FieldName"]}' fid='{DataFields["TemplateFieldId"]}' {(DataFields["Required"] == "1" ? "required" : null)}/>
+                    </div>";
+        }
+    }
+
+    public class NumberInput : IRenderer
+    {
+        public string Draw(Dictionary<string, string> DataFields)
+        {
+            return "This is a number input";
+        }
+    }
+
+};
